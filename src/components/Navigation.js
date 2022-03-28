3fcad064f05d8ed99923ced04aa5c3c50fb43868
OskarRubensson <oskar231@hotmail.com>
2022-03-28 14:35:36 +0200

    Switched to hashrouting

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,9 +8,9 @@ export default function Navigation() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/home">Home</Nav.Link>
-            <Nav.Link href="/pics">Pictures</Nav.Link>
-            <Nav.Link href="/videos">Videos</Nav.Link>
+            <Nav.Link href="#/home">Home</Nav.Link>
+            <Nav.Link href="#/pics">Pictures</Nav.Link>
+            <Nav.Link href="#/videos">Videos</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
