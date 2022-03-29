d8ecddb68bc9b3ac2efcbad30313b76f2cc73dbf
Joel Olofsson (KougatSundew) <jolofsson138@hotmail.com>
2022-03-29 10:00:55 +0200

    Fixed the page with videos

--- src/pages/Videos.js
+++ src/pages/Videos.js
@@ -1,7 +1,24 @@
+import { Container, Form } from "react-bootstrap";
+
+// Fetch all images from images folder
+function importAll(r) {
+  return r.keys().map(r);
+}
+const videos = importAll(require.context('../videos/', false, /\.(mp4|ogg|mov)$/));
+
 export default function Videos() {
-    return (
-      <div>
-        Videos
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Container>
+        <h1>Videos</h1>
+        {videos.map((video, index) => {
+          return (
+            <div key={index}>
+              <video src={video} controls width="100%" />
+            </div>
+          );
+        })}
+      </Container>
+    </div>
+  );
+}  
